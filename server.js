503e88d0f51b40b5b721e16dab46e00429c6febc
jonriddick <jonathansyerriddick@gmail.com>
2017-10-03 18:01:20 -0400

    Added boiler plate code to server.js file

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
- var express = require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
